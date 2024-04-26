c8f188856d6b05e627e314e0774f9c506a5d338e
AlexanderSuarez-Ksquare <Alexander.Suarez@itksquare.edu.mx>
2024-04-25 23:17:10 -0600

    Definición de estilos básicos

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 const app = express();
 const Campground = require("./models/campground");
 
@@ -9,6 +10,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
 
 main().catch((err) => console.log(err));
 
--- seeds/index.js
+++ seeds/index.js
@@ -16,9 +16,14 @@ const seedsDB = async function () {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(places)} ${sample(descriptors)}`,
+      image: "https://random.imagecdn.app/500/150",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed ad maiores quia voluptas recusandae doloremque dignissimos quidem eum aliquid rerum mollitia, ipsam quos, nostrum dolorum, voluptate repellendus? Inventore, explicabo quibusdam.",
+      price: price,
     });
     await camp.save();
   }
